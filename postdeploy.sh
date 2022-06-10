f0f018f5b3786dc7127eeae7de86fc25da95dcf7
Josh Kofsky <jkofsky@salesforce.com>
2022-06-10 14:16:27 -0400

    Adjusting image with playwright dependencies

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,1 +1,3 @@
 python manage.py migrate --noinput
+
+rfbrowser init --skip-browsers; mkdir -p /app/.heroku/python/lib/python3.9/site-packages/Browser/wrapper/node_modules/playwright-core/.local-browsers/chromium-1005/chrome-linux; ln -s /app/.apt/usr/bin/google-chrome /app/.heroku/python/lib/python3.9/site-packages/Browser/wrapper/node_modules/playwright-core/.local-browsers/chromium-1005/chrome-linux/chrome
